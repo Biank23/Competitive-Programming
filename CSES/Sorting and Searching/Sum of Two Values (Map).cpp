1edbe225e20d09c942c2423c296384e86d9ef547
Bianca Vicente <76074424+Biank23@users.noreply.github.com>
2024-02-23 15:58:01 -0300

    Create Sum of Two Values (Map).cpp

---
+++ CSES/Sorting and Searching/Sum of Two Values (Map).cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int n, target;
+    cin >> n >> target;
+    map<int, int> idx;
+    bool found = false;
+    for (int i = 1; i <= n; i++) {
+        int a;
+        cin >> a;
+        if (idx.count(target - a)) {
+            cout << idx[target - a] << ' ' << i << '\n';
+            found = true;
+            break;
+        }
+        idx[a] = i;
+    }
+    if (!found) {
+        cout << "IMPOSSIBLE\n";
+    }
+    
+    return 0;
+}
