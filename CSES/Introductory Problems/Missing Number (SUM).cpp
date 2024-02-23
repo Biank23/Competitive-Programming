9a65a7ea360392c2c49458abd6bb0aed92741d68
Bianca Vicente <76074424+Biank23@users.noreply.github.com>
2024-02-23 13:42:15 -0300

    Create Missing Number (SUM).cpp

---
+++ CSES/Introductory Problems/Missing Number (SUM).cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+using ll = long long;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int n;
+    cin >> n;
+    ll tot = n * (n + 1LL) / 2;
+    ll sum = 0;
+    for (int i = 0; i < n - 1; i++) {
+        int a;
+        cin >> a;
+        sum += a;
+    }
+    cout << tot - sum << '\n';
+    
+    return 0;
+}
