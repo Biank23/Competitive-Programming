aecd71705400f2be601b66bfdf6514119cbb865b
Bianca Vicente <76074424+Biank23@users.noreply.github.com>
2024-02-23 02:37:15 -0300

    Create Digit Queries.cpp

---
+++ CSES/Introductory Problems/Digit Queries.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+using ll = long long;
+ 
+int getDigit(ll k) {
+    int digits = 1;
+    ll cant = 9;
+    ll power_of_10 = 1;
+    while (digits * cant < k) {
+        k -= digits * cant;
+        cant *= 10;
+        power_of_10 *= 10;
+        digits++;
+    }
+    ll number = power_of_10 + (k - 1) / digits;
+    int digit = to_string(number)[(k - 1) % digits] - '0';
+    return digit;
+}
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int q;
+    cin >> q;
+    
+    while (q--) {
+        ll k; cin >> k;
+        cout << getDigit(k) << '\n';
+    }
+    
+    return 0;
+}
