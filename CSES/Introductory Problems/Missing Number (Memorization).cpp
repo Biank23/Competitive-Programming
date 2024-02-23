9546ede559502cb9b077e20f1ada332575f0f722
Bianca Vicente <76074424+Biank23@users.noreply.github.com>
2024-02-23 13:55:06 -0300

    Create Missing Number (Memorization).cpp

---
+++ CSES/Introductory Problems/Missing Number (Memorization).cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int n;
+    cin >> n;
+    vector<bool> missing(n + 1, true); 
+    for (int i = 0; i < n - 1; i++) {
+        int a;
+        cin >> a;
+        missing[a] = false;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (missing[i]) cout << i << '\n';
+    }
+    
+    return 0;
+}
