79ddf0f5741706e3c9d18e01820f38170b26a2d1
Bianca Vicente <76074424+Biank23@users.noreply.github.com>
2024-02-23 00:19:53 -0300

    Create Missing Number.cpp

---
+++ CSES/Introductory Problems/Missing Number.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int n;
+    cin >> n;
+    int x = 0;
+    for (int i = 1; i <= n; i++) {
+        x ^= i;
+    }
+    for (int i = 0; i < n - 1; i++) {
+        int a;
+        cin >> a;
+        x ^= a;
+    }
+    cout << x << '\n';
+    
+    return 0;
+}
