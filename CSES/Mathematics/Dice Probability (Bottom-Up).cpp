15b54bc2eb0f3af2907a9b37462b7c0694d7a602
Bianca Vicente <76074424+Biank23@users.noreply.github.com>
2024-02-26 01:54:45 -0300

    Update Dice Probability (Bottom-Up).cpp

--- CSES/Mathematics/Dice Probability (Bottom-Up).cpp
+++ CSES/Mathematics/Dice Probability (Bottom-Up).cpp
@@ -7,11 +7,11 @@ const int MAX_N = 101, MAX_X = 601;
 double dp[MAX_N][MAX_X];
  
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     
-	int n, a, b;
-	cin >> n >> a >> b;
+    int n, a, b;
+    cin >> n >> a >> b;
     
     dp[0][0] = 1.0;
     for (int i = 1; i <= n; i++) {
@@ -22,12 +22,12 @@ int main() {
         }
     }
 	
-	double ans = 0.0;
+    double ans = 0.0;
     for (int i = a; i <= b; i++) {
         ans += dp[n][i];
     }
 	
-	cout << setprecision(6) << fixed << ans << '\n';
+    cout << setprecision(6) << fixed << ans << '\n';
 	
-	return 0;
+    return 0;
 }
