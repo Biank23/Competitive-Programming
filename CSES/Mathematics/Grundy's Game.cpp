96389105dbfbbbc5f914f985e7985cf305d8913c
Bianca Vicente <76074424+Biank23@users.noreply.github.com>
2024-02-26 02:05:24 -0300

    Update Grundy's Game.cpp

--- CSES/Mathematics/Grundy's Game.cpp
+++ CSES/Mathematics/Grundy's Game.cpp
@@ -29,6 +29,68 @@ int main() {
 	
     init();
     int t; cin >> t;
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int MAX_N = 1300;
+ 
+int mex(set<int> &s) {
+    for (int i = 0; i < MAX_N; i++) {
+        if (!s.count(i)) return i;
+	}
+	assert(false);
+}
+ 
+int dp[MAX_N];
+ 
+void init() {
+    for (int i = 0; i < MAX_N; i++) {
+        set<int> s;
+        for (int j = 1; j < (i + 1) / 2; j++) {
+            s.insert(dp[j] ^ dp[i - j]);
+        }
+        dp[i] = mex(s);
+    }
+}
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+	
+    init();
+    int t; cin >> t;
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int MAX_N = 1300;
+ 
+int mex(set<int> &s) {
+    for (int i = 0; i < MAX_N; i++) {
+        if (!s.count(i)) return i;
+	}
+	assert(false);
+}
+ 
+int dp[MAX_N];
+ 
+void init() {
+    for (int i = 0; i < MAX_N; i++) {
+        set<int> s;
+        for (int j = 1; j < (i + 1) / 2; j++) {
+            s.insert(dp[j] ^ dp[i - j]);
+        }
+        dp[i] = mex(s);
+    }
+}
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+	
+    init();
+    int t; cin >> t;
     while (t--) {
         int n;
         cin >> n;
@@ -37,7 +99,6 @@ int main() {
         } else {
             cout << "second\n";
         }
-	}
-	
-	return 0;
+    }
+    return 0;
 }
