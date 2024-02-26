4419c4d6cb9434692090adb2a347ca10a33d0f4e
Bianca Vicente <76074424+Biank23@users.noreply.github.com>
2024-02-26 01:59:06 -0300

    Update Candy Lottery.cpp

--- CSES/Mathematics/Candy Lottery.cpp
+++ CSES/Mathematics/Candy Lottery.cpp
@@ -3,17 +3,17 @@
 using namespace std;
  
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
 	
-	double n, k;
-	cin >> n >> k;
+    double n, k;
+    cin >> n >> k;
 	
-	double ans = 0.0;
-	for (int i = 1; i <= k; i++) {
-		ans += (pow(i / k, n) - pow((i - 1) / k, n)) * i;
-	}
-	cout << setprecision(6) << fixed << ans << '\n';
+    double ans = 0.0;
+    for (int i = 1; i <= k; i++) {
+        ans += (pow(i / k, n) - pow((i - 1) / k, n)) * i;
+    }
+    cout << setprecision(6) << fixed << ans << '\n';
 	
-	return 0;
+    return 0;
 }
